bae08f98cd602c448c7487f1be4b1c4665c17ba7
Lutfi Maulana <89976129+Lutfi01@users.noreply.github.com>
2022-02-27 12:30:32 +0700

    Add files via upload

---
+++ tugas2.java
@@ -0,0 +1,13 @@
+public class tugas2 {
+    
+    public static void main(String[] args) {
+        String[] nama = {"L","A","N","A"};
+     
+        //no 2
+        //Tentukan hasil yang didapatkan ketika dilakukan perintah berikut ini:
+        //size()
+     
+        System.out.println("ukuran array adalah " + nama.length);     
+        
+    }
+    }
---
+++ tugas4.java
@@ -0,0 +1,21 @@
+import java.util.ArrayList;
+
+public class tugas4 {
+    public static void main(String[] args) {
+    //String[] nama = {"L","A","N","A"};
+    ArrayList<String> nama = new ArrayList<String>();
+    nama.add("L");
+    nama.add("A");
+    nama.add("N");
+    nama.add("A");
+    
+    //no 4
+    //Tentukan hasil yang didapatkan ketika dilakukan perintah berikut ini:
+    //indexOf(a), indexOf(c), indexOf(q)
+
+    System.out.println(nama.indexOf("a"));
+    System.out.println(nama.indexOf("c"));
+    System.out.println(nama.indexOf("q"));
+
+    }
+}
